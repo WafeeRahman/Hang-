22eca2f352de038bccfe1f6c371f5d774dd89c32
WafeeRahman <wafeerahman84@gmail.com>
2023-09-29 20:01:10 -0400

    Preamble
    - Install Mongoose
    - Install Nodejs
    - Install Express
    - Create and Connect DB
    - Initialize RESTful Routes
    -

---
+++ app.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose'); //Req Mongoose
+const path = require('path')
+
+//Connect to Mongoose
+
+mongoose.connect('mongodb://127.0.0.1:27017/court-grounds', {
+    useNewUrlParser : true, 
+    useUnifiedTopology: true,
+});
+
+
+const db = mongoose.connection; //shorthand for db
+
+db.on('error', console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("MongoDB Connected");
+})
+
+
+
+
+//Initialize Express and RESTful Routes
+const express = require('express');
+const app = express();
+
+
+
+app.set('view engine', 'ejs');
+app.set('/views', path.join(__dirname, 'views'));
+
+app.get('/', (req,res) => {
+    res.render('home')
+})
+
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
---
+++ models/courtground.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; //Schema Constant for Shorthand
+
+
+
+//Schema for Court Data Model
+const courtSchema = new Schema({
+    title: String,
+    description: String,
+    price: String,
+    location: String
+
+});
+
+// Export Court Model to Make Available to Other Files
+module.exports = mongoose.model('courtModel', courtSchema)
