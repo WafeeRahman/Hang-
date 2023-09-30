88264097adf347fa366200f6245f5f9f3fc6b0b4
WafeeRahman <wafeerahman84@gmail.com>
2023-09-30 01:33:37 -0400

    Implented CRUD Functionality
    - Update and Delete Functionality for Data
    - Added Middleware that describes each Request
    - Added Morgan Dependency
    - Created Edit and Update Pages and Traversal Functionalities

--- app.js
+++ app.js
@@ -18,16 +18,24 @@ db.once("open", () => {
 
 
 
-
 //Initialize Express and RESTful Routes
 
 const express = require('express');
 const app = express();
-
+const methodOverride = require('method-override');
 
 
 app.set('view engine', 'ejs');
 app.set('/views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true })); // Parses Pages
+app.use(methodOverride('_method')) //Overwrites HTML methods for PATCHING
+
+app.use((req, res, next) => {
+    console.log(req.method, req.path)
+    next();
+})
+
 
 app.get('/', (req, res) => {
 
@@ -36,34 +44,67 @@ app.get('/', (req, res) => {
 });
 
 
-
+// Takes users to Read all SpotGrounds in DB
 app.get('/spotgrounds', async (req, res) => {
     const spotGrounds = await spotGround.find({});
     res.render('spotgrounds/index', { spotGrounds });
 
 });
 
-app.get('/spotgrounds/new', (req,res) => {
+
+// Allows users to CREATE a new page with a form that sends a POST request to the spotGROUNDS page
+app.get('/spotgrounds/new', (req, res) => {
+
     res.render('spotgrounds/new');
-});
-
-
-app.post('/spotgrounds', async(req, res) {
-    res.send(req.body)
 
 });
 
+// Allows users to CREATE a page with a post request
+app.post('/spotgrounds', async (req, res) => {
+    const spot = new spotGround(req.body.spotgrounds); //Forms create a new spotground object
+    await spot.save(); //Save to DB
+    res.redirect(`/spotgrounds/${spot._id}`) //Redirects to details page
+
+});
+
+// Allows users to READ an existing spotGround page in more detail
 app.get('/spotgrounds/:id', async (req, res) => {
 
     const id = req.params.id;
     const spot = await spotGround.findById(id);
-    res.render('spotgrounds/show', {spot});
-      
+    res.render('spotgrounds/show', { spot });
+
 });
 
+// Allows users to UPDATE SpotsGrounds within the dataBase using method override and HTML put requests
+app.put('/spotgrounds/:id', async (req, res) => {
+    const id = req.params.id;
+    const spot = await spotGround.findByIdAndUpdate(id, { ...req.body.spotgrounds }, { new: true }); // Spread req body into the database object with matching id
+    res.redirect(`/spotgrounds/${spot._id}`) //Redirects to details page
 
+
+});
+
+// Allows users to fill forms to UPDATE spotGround
+
+app.get('/spotgrounds/:id/edit', async (req, res) => {
+
+    const id = req.params.id;
+    const spot = await spotGround.findById(id);
+    res.render('spotgrounds/edit', { spot });
+
+});
+
+app.delete('/spotgrounds/:id', async (req, res) => {
+
+    const { id } = req.params;
+    const deleted = await spotGround.findByIdAndDelete(id);
+    res.redirect(`/spotgrounds`)
+
+});
 
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
 });
+
